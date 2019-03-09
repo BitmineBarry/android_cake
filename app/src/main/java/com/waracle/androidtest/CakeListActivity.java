64ee0548497a95ac9c0e52ee274325b358104e4d
Barry Walsh <barry@netwidecomputing.co.uk>
2019-03-09 20:14:19 +0000

    rename the cake list activity layout

--- app/src/main/java/com/waracle/androidtest/CakeListActivity.java
+++ app/src/main/java/com/waracle/androidtest/CakeListActivity.java
@@ -19,7 +19,7 @@ public class CakeListActivity extends Activity implements CakeListFragment.OnLis
 
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.cake_list_activity);
 
         // setup cache management for HTTP/HTTPS for this application
         try {
